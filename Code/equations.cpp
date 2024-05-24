8e9a597de0565df73ad3f5387d74eab8547ee3a5
MitchellZeleny <135643132+MitchellZeleny@users.noreply.github.com>
2024-05-24 00:58:22 -0500

    Update equations.cpp
    completely redid the project again as I was stuck

--- Code/equations.cpp
+++ Code/equations.cpp
@@ -1,135 +1,138 @@
 #include <iostream>
 #include <cmath>
-using namespace std; 
+using namespace std;
 
 int main()
 {
-	// Variables needed
-    double num1; 
-    double num2;  
-    double answer; 
-    int solve; 
-    int size; 
+	string contCalc;
+	double answer;
+	double num1;
+	double num2;
+	double num3;
+	double num4;
+	int calc;
+	double finalAnswer;
 
-    // User Interface
-    cout << "Using PEMDAS, properly order your equation in order of operations needed to be performed to get the correct answer." << endl; 
-    cout << "How many calculations are needed to be performed?" << endl; 
-    cout << "For example: 2(3 * 2) + 1 is 3 calculations." << endl; 
-    cin >> size; 
-    cout << "Enter a numerical value." << endl; 
-    cin >> num1; 
-    cout << "Enter the operation you want to be performed on the first value" << endl; 
-    cout << "Enter 1 for addition." << endl; 
-    cout << "Enter 2 for subtrction." << endl; 
-    cout << "Enter 3 for multiplcation." << endl; 
-    cout << "Enter 4 for division." << endl; 
-    cout << "Enter 5 for the first value to the power of the second." << endl; 
-    cout << "Enter 6 for the square root of the value you just entered." << endl; 
-    cin >> solve; 
-    cout << "If you only need the square root of the first value, just enter 0 for the second value." << endl; 
-    cout << "Enter the second numerical value." << endl; 
-    cin >> num2; 
+	cout << "Enter the first number you need to calculate: " << endl;
+	cin >> num1;
+	cout << "What operation would you like to perform to " << num1 << "?" << endl;
+	cout << "Enter 1 for addition" << endl;
+	cout << "Enter 2 for subtraction" << endl;
+	cout << "Enter 3 for multiplication" << endl;
+	cout << "Enter 4 for division" << endl;
+	cout << "Enter 5 for square root of " << num1 << endl;
+	cout << "Enter 6 for " << num1 << " to the power of another number" << endl;
+	cin >> calc;
 
-    // Calculations 
-    if (solve == 1)
-    {
-        answer = num1 + num2; 
-        cout << "The answer is: " << answer << endl; 
-    }
-    else if (solve == 2)
-    {
-        answer = num1 - num2;
-        cout << "The answer is: " << answer << endl; 
-    }
-    else if (solve == 3)
-    {
-        answer = num1 * num2;
-        cout << "The answer is: " << answer << endl; 
-    }
-    else if (solve == 4)
-    {
-        answer = num1 / num2;
-        cout << "The answer is: " << answer << endl; 
-    }
-    else if (solve == 5)
-    {
-        answer = pow(num1, num2);
-        cout << "The answer is: " << answer << endl;  
-    }
-    else if (solve == 6)
-    {
-        answer = sqrt(num1);
-        cout << "The answer is: " << answer << endl; 
-    }
-    else
-    {
-        cout << "Invalid input, try again." << endl;
-        cin >> solve; 
-    }
+	if (calc == 1)
+	{
+		cout << "Enter the second number you need to add to " << num1 << endl;
+		cin >> num2;
+		answer = num1 + num2;
+		cout << " Your answer is: " << answer << endl;
+		answer = num3;
+	}
+	else if (calc == 2)
+	{
+		cout << "Enter the second number you need to subtract from " << num1 << endl;
+		cin >> num2;
+		answer = num1 - num2;
+		cout << " Your answer is: " << answer << endl;
+		answer = num3;
+	}
+	else if (calc == 3)
+	{
+		cout << "Enter the second number you need to multiply " << num1 << " by" << endl;
+		cin >> num2;
+		answer = num1 * num2;
+		cout << " Your answer is: " << answer << endl;
+		answer = num3;
+	}
+	else if (calc == 4)
+	{
+		cout << "Enter the second number you need to divide " << num1 << " by" << endl;
+		cin >> num2;
+		answer = num1 / num2;
+		cout << " Your answer is: " << answer << endl;
+		answer = num3;
+	}
+	else if (calc == 5)
+	{
+		answer = sqrt(num1);
+		cout << " Your answer is: " << answer << endl;
+		answer = num3;
+	}
+	else if (calc == 6)
+	{
+		cout << "Enter the second number you need to raise " << num1 << " to the power of" << endl;
+		cin >> num2;
+		answer = pow(num1, num2);
+		cout << "Your answer is: " << answer << endl;
+		answer = num3;
+	}
+	else
+	{
+		cout << "Invalid entry, please try again." << endl;
+	}
 
-    // Create boolean value to see if more calculations are needed
-    // Loop if yes, exit if no
-
-    bool moreCalc = true;
-    cout << "Do you need to perform another calculation to this value? " << endl;
-    cout << "Enter 1 for yes, 0 for no." << endl; 
-    cin >> moreCalc;
-    if (moreCalc == 1)
-    {
-        cout << "Enter the operation you want to be performed on the first value" << endl; 
-        cout << "Enter 1 for addition." << endl;
-        cout << "Enter 2 for subtrction." << endl; 
-        cout << "Enter 3 for multiplcation." << endl; 
-        cout << "Enter 4 for division." << endl; 
-        cout << "Enter 5 for the first value to the power of the second." << endl; 
-        cout << "Enter 6 for the square root of the value you just entered." << endl; 
-        cout << "If you only need the square root of " << answer << ", just enter 0 for the second value." << endl;
-        cin >> solve; 
-        cout << "Enter the numerical value you want to perform on " << answer << endl; 
-        cin >> num2;
-        if (solve == 1)
-        {
-            answer = answer + num2; 
-        }
-        else if (solve == 2)
-        {
-            answer = answer - num2;
-        }
-        else if (solve == 3)
-        {
-            answer = answer * num2;
-        }
-        else if (solve == 4)
-        {
-            answer = answer / num2;
-        }
-        else if (solve == 5)
-        { 
-            answer = pow(answer, num2);
-        }
-        else if (solve == 6)
-        {
-            answer = sqrt(answer); 
-        }
-        else
-        {
-            cout << "Invalid input, try again." << endl;
-            cin >> solve;
-        }
-
-        cout << "The answer is: " << answer << endl;
-        cout << "Do you need to perform another calculation to this value? " << endl; 
-        cout << "Enter 1 for yes, 0 for no." << endl; 
-        cin >> moreCalc; 
-    }
-    else if (moreCalc == 0)
-    {
-        cout << "Your final answer is: " << answer << endl; 
-    }
-    else
-    {
-		cout << "Invalid input, try again." << endl;  
-		cin >> moreCalc;  
+	do
+	{
+		cout << "Do you need to continue calculations on " << answer << "?" << endl;
+		cout << "Enter 'yes' to continue" << endl;
+		cin >> contCalc;
+	} 
+	while (contCalc == "yes");
+	cout << "What operation would you like to perform to " << num3 << "?" << endl;
+	cout << "Enter 1 for addition" << endl;
+	cout << "Enter 2 for subtraction" << endl;
+	cout << "Enter 3 for multiplication" << endl;
+	cout << "Enter 4 for division" << endl;
+	cout << "Enter 5 for square root of " << num3 << endl;
+	cout << "Enter 6 for " << num3 << " to the power of another number" << endl;
+	cin >> calc;
+	if (calc == 1)
+	{
+		cout << "Enter the second number you need to add to " << num3 << endl;
+		cin >> num4;
+		finalAnswer = num3 + num4;
+		cout << " Your answer is: " << finalAnswer << endl;
+	}
+	else if (calc == 2)
+	{
+		cout << "Enter the second number you need to subtract from " << num3 << endl;
+		cin >> num4;
+		finalAnswer = num3 - num4;
+		cout << " Your answer is: " << finalAnswer << endl;
+	}
+	else if (calc == 3)
+	{
+		cout << "Enter the second number you need to multiply " << num3 << " by" << endl;
+		cin >> num4;
+		finalAnswer = num3 * num4;
+		cout << " Your answer is: " << finalAnswer << endl;
+	}
+	else if (calc == 4)
+	{
+		cout << "Enter the second number you need to divide " << num3 << " by" << endl;
+		cin >> num4;
+		finalAnswer = num3 / num4;
+		cout << " Your answer is: " << finalAnswer << endl;
+	}
+	else if (calc == 5)
+	{
+		finalAnswer = sqrt(num3);
+		cout << " Your answer is: " << finalAnswer << endl;
+	}
+	else if (calc == 6)
+	{
+		cout << "Enter the second number you need to raise " << num3 << " to the power of" << endl;
+		cin >> num4;
+		finalAnswer = pow(num3, num4);
+		cout << "Your answer is: " << finalAnswer << endl;
+	}
+	else
+	{
+		cout << "Invalid entry, please try again." << endl;
 	}
 
 
